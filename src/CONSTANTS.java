c5f9cc2e8a3eb408a40547b7f1bf5e60867e5420
Quay Gieng <qgieng@uci.edu>
2021-07-08 23:06:23 -0700

    initial file set up

---
+++ src/CONSTANTS.java
@@ -0,0 +1,9 @@
+
+public class CONSTANTS {
+	
+	public final int LDISK = 64;
+	//16 integers
+	public final int BLOCK_SIZE = 64;
+	//4 integers, 1 file, 3 block#
+	public final int DESCRIPTOR_SIZE = 4;
+}
---
+++ src/User.java
@@ -0,0 +1,27 @@
+
+public class User {
+	User(){
+		
+	}
+	
+	public void create(String filename) {
+		
+	}
+	
+	public void destroy(String filename) {
+		
+	}
+	public void open(String filename) {
+		
+	}
+	public void read(int index, int mem_area, int count) {
+		
+	}
+	
+	public void write(int index, int mem_area, int count) {
+		
+	}
+	public void lseek(int index, int pos) {
+		
+	}
+}
